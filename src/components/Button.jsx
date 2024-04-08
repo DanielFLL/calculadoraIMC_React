3fdfaf219ba39fee263c5b293a49c19010ba7511
DanielFLL <daniel-dfl@hotmail.com>
2024-04-08 15:43:53 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import "./Button.css";
+
+const Button = ({ id, text, action }) => {
+  const handleAction = (e) => {
+    action(e);
+  };
+
+  return (
+    <button id={id} onClick={handleAction}>
+      {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  action: PropTypes.string.isRequired,
+};
+
+export default Button;
